82deb93f16f82ce739c473e6facc0897af02ab37
NguyenThiLanh63134427 <nguyentlanh220703@gmail.com>
2024-04-26 14:15:35 +0700

    Hoàn thành bài BottomNavigation

---
+++ BottomNavigation/app/src/main/java/ntu/nguyenthilanh_63134427/bottomnavigation/MainActivity.java
@@ -0,0 +1,52 @@
+package ntu.nguyenthilanh_63134427.bottomnavigation;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_navigation), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            int itemId = menuItem.getItemId();
+            if (itemId == R.id.home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.friend) {
+                selectedFragment = new FriendFragment();
+            } else if (itemId == R.id.message) {
+                selectedFragment = new MessageFragment();
+            }
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+            return false;
+        }
+    };
+}
